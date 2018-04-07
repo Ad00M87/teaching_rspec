ed89ff48147080a0dd1bafaf1ff6b21fed64d9a2
Adam Kimball <adamchasekimball@gmail.com>
2018-04-06 22:49:41 -0600

    fixed issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,11 +40,8 @@ end
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
- config.include FactoryBot::Syntax::Methods
-end
-
-RSpec.configure do |config|
   # Devise Controller Spec Configuration
+  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
 
